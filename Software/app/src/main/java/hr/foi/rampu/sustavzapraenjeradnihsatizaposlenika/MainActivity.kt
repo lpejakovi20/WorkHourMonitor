9948402d8ceedd084a8d59a96c212b9039d29652
Lovro Pejakovic <lpejakovi20@foi.hr>
2022-11-16 19:41:48 +0100

    Prepravci u dizajnu.

--- Software/app/src/main/java/hr/foi/rampu/sustavzapraenjeradnihsatizaposlenika/MainActivity.kt
+++ Software/app/src/main/java/hr/foi/rampu/sustavzapraenjeradnihsatizaposlenika/MainActivity.kt
@@ -1,11 +1,27 @@
 package hr.foi.rampu.sustavzapraenjeradnihsatizaposlenika
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gumb = findViewById<Button>(R.id.btn_registration)
+        gumb.setOnClickListener {
+            val intent = Intent(this, Registration::class.java)
+            startActivity(intent)
+        }
+
+        val hyper = findViewById<TextView>(R.id.tv_password_reset)
+        hyper.setOnClickListener {
+            val intent = Intent(this, PasswordReset::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ Software/app/src/main/java/hr/foi/rampu/sustavzapraenjeradnihsatizaposlenika/PasswordReset.kt
@@ -0,0 +1,11 @@
+package hr.foi.rampu.sustavzapraenjeradnihsatizaposlenika
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PasswordReset : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_reset)
+    }
+}
